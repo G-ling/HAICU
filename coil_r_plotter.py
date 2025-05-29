8ccaabd6bc80d0b32d0e5c41cb724010d97efd9c
Giles <gw440@cam.ac.uk>
2025-05-29 12:39:56 -0700

    edit

--- coil_r_plotter.py
+++ coil_r_plotter.py
@@ -10,7 +10,7 @@ from datetime import datetime
 dates = [datetime(2025, 2, 3), datetime(2025, 5, 2), datetime(2025, 5, 20)]
 values = [25.6, 29.7, 30.0]
 plt.figure()
-plt.plot(mdates.date2num(dates), values, marker = '.', mfc = 'k', mec = 'k', linewidth = 1.5)
+plt.errorbar(mdates.date2num(dates), values, yerr = 0.04, marker = '.', mfc = 'k', mec = 'k', linewidth = 1.5)
 plt.ylabel('Resistance (m$\Omega$)')
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%b-%d'))
 plt.gcf().autofmt_xdate()
