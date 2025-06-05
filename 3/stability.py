6034a50c118d6cb605841be92b2bbad6820b5dce
Giles <gw440@cam.ac.uk>
2025-06-05 16:32:01 -0700

    u

--- 3/stability.py
+++ 3/stability.py
@@ -19,6 +19,18 @@ ax.set_ylabel('R$_0$ (m$\Omega$ / $^0 C$)')
 plt.show()
 
 fig,ax = plt.subplots()
-plt.plot(times,1000*data['R1_t'])
+plt.plot(times,data['in'],label = 'In')
+plt.plot(times,data['out'], label = 'Out')
+plt.plot(times,data['big1'], label = 'Coil 2 (a)')
+plt.plot(times,data['big2'], label = 'Coil 2 (b)')
+plt.plot(times,data['small1'], label = 'Coil 0 (a)')
+plt.plot(times,data['small2'], label = 'Coil 0 (b)')
+ax.set_ylabel('Temperature ($^0 C$)')
+
+plt.legend()
+
+
+#ax2 = ax.twinx()
+#ax2.plot(times,1000*data['curr'])
 
 plt.show()
