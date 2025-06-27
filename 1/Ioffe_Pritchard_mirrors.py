33515a43dea1ba4692ac90573ce61368808f53c2
Giles <gw440@cam.ac.uk>
2025-06-27 16:27:09 -0700

    chukman comments mod

--- 1/Ioffe_Pritchard_mirrors.py
+++ 1/Ioffe_Pritchard_mirrors.py
@@ -13,10 +13,10 @@ def on_axis(R,I,center,neg = 1):
     y = [(neg)*(I*(R**2))/(((R**2) + ((i-center)**2))**(3/2)) for i in x]
     return y
 
-a = on_axis(1,1,3)#gaussian_points(3,30,6) 
-b = on_axis(1,1,-3)#gaussian_points(3,30,-6)
-c = on_axis(3,1,2,1)#+1*gaussian_points(1,10,2)
-d = on_axis(3,1,-2,1)#+1*gaussian_points(1,10,-2)
+a = on_axis(2,1,3)#gaussian_points(3,30,6) 
+b = on_axis(2,1,-3)#gaussian_points(3,30,-6)
+c = on_axis(2,.4,2,1)#+1*gaussian_points(1,10,2)
+d = on_axis(2,.4,-2,1)#+1*gaussian_points(1,10,-2)
 e = list(map(add,a,b))
 e = list(map(add,e,c))
 e = list(map(add,e,d))
@@ -30,16 +30,16 @@ ax.plot(np.linspace(-10,10,num=3000), c, color = 'xkcd:dark pink', linestyle = '
 ax.plot(np.linspace(-10,10,num=3000), d, color = 'xkcd:dark pink', linestyle = '--')
 ax.plot(np.linspace(-10,10,num=3000), e, color = 'xkcd:dark teal')
 
-ax.set_title("Outer + Inner coils", fontsize = 14)
+ax.set_title("Ioffe-Pritchard", fontsize = 14)
 
 #ax.get_xaxis().set_visible(False)
 #ax.get_yaxis().set_visible(False)
 
 
 
-ax.set_xlabel("Axial displacement, $z$", fontsize = 14)
-start, end = ax.get_xlim()
-ax.xaxis.set_ticks(np.arange(start+1, end, 5))
-ax.set_ylabel("$B_{total}$", fontsize = 14)
+#ax.set_xlabel("Axial displacement, $z$", fontsize = 14)
+#start, end = ax.get_xlim()
+#ax.xaxis.set_ticks(np.arange(start+1, end, 5))
+#ax.set_ylabel("$B_{total}$", fontsize = 14)
 
 plt.show()
