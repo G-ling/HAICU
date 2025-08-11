8f645e63b5945a39c6e1cfb5555c7ebf37c93290
Giles <gw440@cam.ac.uk>
2025-08-11 11:43:18 -0700

    thesis final rev

---
+++ 3/Ioffe_Pritchard_mirrors_thesis.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from operator import add
+
+def gaussian_points(height, width, mean):
+    x = np.linspace(-15,15,num=3000)
+    y = height*np.exp(-((x-mean)**2)/width)
+    return y
+
+def on_axis(R,I,center,neg = 1):
+    x = np.linspace(-10,10,num=3000)
+    y = [(neg)*(I*(R**2))/(((R**2) + ((i-center)**2))**(3/2)) for i in x]
+    return y
+
+a = on_axis(2,1,3)#gaussian_points(3,30,6) ##2,1,3
+b = on_axis(2,1,-3)#gaussian_points(3,30,-6) ##2,1,-3
+c = on_axis(2,.4,2,1)#+1*gaussian_points(1,10,2) ##2,.4,2,1
+d = on_axis(2,.4,-2,1)#+1*gaussian_points(1,10,-2) ##2,.4,-2,1
+e = list(map(add,a,b))
+e = list(map(add,e,c))
+e = list(map(add,e,d))
+
+#plt.rcParams.update({'font.size': 12})
+
+fig, (ax,ax2) = plt.subplots(2,sharex=True)
+ax.plot(np.linspace(-10,10,num=3000), a, color = 'xkcd:ocean blue', linestyle = '--', lw=1)
+ax.plot(np.linspace(-10,10,num=3000), b, color = 'xkcd:ocean blue', linestyle = '--', lw=1)
+ax.plot(np.linspace(-10,10,num=3000), c, color = 'xkcd:dark pink', linestyle = '--', lw=1)
+ax.plot(np.linspace(-10,10,num=3000), d, color = 'xkcd:dark pink', linestyle = '--', lw=1)
+ax.plot(np.linspace(-10,10,num=3000), e, color = 'xkcd:dark teal', lw=1)
+
+#ax.set_title("Modified (anti-Helmholtz)", fontsize = 14)
+ax.set_title("Ioffe-Pritchard", fontsize = 10)
+#ax.get_xaxis().set_visible(False)
+#ax.get_yaxis().set_visible(False)
+
+
+
+
+#ax.set_xlabel("Axial displacement, $z$", fontsize = 14)
+start, end = ax.get_xlim()
+ax.xaxis.set_ticks(np.arange(start+1, end, 5))
+ax.set_ylabel("$B_{z}$", fontsize = 10)
+
+ax.axvspan(-1.5,-3, color='xkcd:red', alpha=0.3)
+
+
+a2 = on_axis(2,1,3)#gaussian_points(3,30,6) ##2,1,3
+b2 = on_axis(2,1,-3)#gaussian_points(3,30,-6) ##2,1,-3
+c2 = on_axis(2,.4,2,-1)#+1*gaussian_points(1,10,2) ##2,.4,2,1
+d2 = on_axis(2,.4,-2,-1)#+1*gaussian_points(1,10,-2) ##2,.4,-2,1
+e2 = list(map(add,a2,b2))
+e2 = list(map(add,e2,c2))
+e2 = list(map(add,e2,d2))
+
+ax2.plot(np.linspace(-10,10,num=3000), a2, color = 'xkcd:ocean blue', linestyle = '--', lw = 1)
+ax2.plot(np.linspace(-10,10,num=3000), b2, color = 'xkcd:ocean blue', linestyle = '--', lw = 1)
+ax2.plot(np.linspace(-10,10,num=3000), c2, color = 'xkcd:dark pink', linestyle = '--', lw = 1)
+ax2.plot(np.linspace(-10,10,num=3000), d2, color = 'xkcd:dark pink', linestyle = '--', lw = 1)
+ax2.plot(np.linspace(-10,10,num=3000), e2, color = 'xkcd:dark teal', lw = 1)
+
+ax2.set_title("Modified (anti-Helmholtz)", fontsize = 10)
+#ax.set_title("Ioffe-Pritchard", fontsize = 14)
+#ax.get_xaxis().set_visible(False)
+#ax.get_yaxis().set_visible(False)
+
+
+
+
+ax2.set_xlabel("Axial displacement, $z$", fontsize = 10)
+start, end = ax2.get_xlim()
+ax2.xaxis.set_ticks(np.arange(start+1, end, 5))
+ax2.set_ylabel("$B_{z}$", fontsize = 10)
+
+ax2.axvspan(-1.5,-3, color='xkcd:red', alpha=0.3)
+
+plt.show()
